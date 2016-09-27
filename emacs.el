ddb5576aa9e21203c50eed58c46182fe5c29abda
Shrutarshi Basu <shr@basus.me>
2016-09-27 19:42:21 -0400

    Cleaned up the configuration entry point

--- emacs.el
+++ emacs.el
@@ -1,52 +1,64 @@
-;; Set file load paths and load additional files
-(setq emacs-root "~/src/emacs/")
-(setq custom-file "~/.emacs.d/custom.el")
+;; Require Common Lisp compatibility
+(require 'cl)
 
-;; Stuff I've pulled from the internet. I require or load them as needed
-;; (labels ((add-path (p)
-;;                     (add-to-list 'load-path
-;;                                 (concat emacs-root p))))
-;;   (add-path "lisp")
-;;   (add-path "net")
-;;   (add-path "net/scala")
-;;   (add-path "net/slime")
-;;   (add-path "net/sml-mode")
-;;   (add-path "net/haskell-mode")
-;;   (add-path "net/org/lisp")
-;;   (add-path "net/org2blog")
-;;   (add-path "net/tuareg")
-;;   (add-path "net/simplenote")
-;;   (add-path "net/php-mode")
-;;   (add-path "net/emacs-color-theme-solarized")
-;; )
-
-
+;; Load local configurations
+(add-to-list 'load-path "~/src/emacs/")
+(load "~/.emacs.d/custom.el")
+(load "packages")
 (load "keys")
-(load "functions")
 (load "modes")
+(load "functions")
+(load "fira.el")
 
-;; Set user interface preferences
-(setq visible-bell t)
-(set-frame-parameter nil 'font-backend '(xft))
+;; User interface preferences
+(when window-system
+  (if (fboundp 'tabbar-mode) (tabbar-mode -1))
+  (tool-bar-mode      -1)
+  (scroll-bar-mode    -1)
+  (blink-cursor-mode  -1)
+  (menu-bar-mode       1)
+  )
 
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;; Miscellaneous behavior
+(setq
+ visible-bell               t
+ vc-follow-symlinks         t
+ x-select-enable-clipboard  t
+ make-backup-files          nil
+ coding-system-for-read     'utf-8
+ coding-system-for-write    'utf-8
+ confirm-kill-emacs         'y-or-n-p
+ initial-major-mode         'fundamental-mode
+)
 
-;; Tab behavior
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+;; Default behaviors
+(setq-default
+ show-trailing-whitespace  t
+ indent-tabs-mode          nil
+ fill-column               80
+ auto-fill-function        'do-auto-fill
+ )
 
-;; Other behavior
-(setq x-select-enable-clipboard t)
-(setq make-backup-files nil)
-(setq-default fill-column 80)
-(setq-default auto-fill-function 'do-auto-fill)
-(setq-default show-trailing-whitespace t)
+;; Global mode settings
+(savehist-mode)
+(show-paren-mode)
+(global-linum-mode)
+(column-number-mode)
+(global-hl-line-mode)
 
-;; Global mode setings
-(show-paren-mode t)
-(global-hl-line-mode t)
-(global-auto-complete-mode t)
-(global-linum-mode t)
-(column-number-mode t)
-(autopair-global-mode)
+;; Aquamacs specific settings
+(if (boundp 'aquamacs-version)
+    (progn
+      (setq shell-command-switch "-lc")
+      (set-default-font "Fira Code 14")
+      (customize-set-variable 'aquamacs-autoface-mode nil)
+      (customize-set-variable 'aquamacs-additional-fontsets nil t)
+      (customize-set-variable 'aquamacs-customization-version-id 307 t)
+      (customize-set-variable 'aquamacs-tool-bar-user-customization nil t)
+      (exec-path-from-shell-initialize))
+
+  (set-default-font "Inconsolata for Powerline 11")
+)
+
+;; Start server
+(server-start)
